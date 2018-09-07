decdd04f98015d0cf4ee0685bedc100a16bc385d
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-09-06 19:13:23 -0500

    Working on Swift package

--- Package.swift
+++ Package.swift
@@ -1,15 +1,11 @@
-// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "AndroidUIKit",
-    dependencies: [
-        //.Package(url: "https://github.com/PureSwift/Android.git", majorVersion: 0)
-        .package(url: "git@github.com:PureSwift/Android.git", .branch("master"))
-    ],
     targets: [
-        .target(name: "AndroidUIKit",
-            dependencies: ["Android"],
-            path: "Sources")
-        ]
+        Target(name: "AndroidUIKit")
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/PureSwift/Android.git", majorVersion: 0)
+    ]
 )
