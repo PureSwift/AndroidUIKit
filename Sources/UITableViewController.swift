c86996bb73d6fb8aa1faf49cf4883042aa0edfcd
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-09-07 01:44:04 -0500

    Added `UITableViewController`

---
+++ Sources/UITableViewController.swift
@@ -0,0 +1,126 @@
+//
+//  UITableView.swift
+//  AndroidUIKit
+//
+//  Created by Alsey Coleman Miller on 9/7/18.
+//  Copyright © 2018 PureSwift. All rights reserved.
+//
+
+import Foundation
+import Android
+
+open class UITableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    public private(set) var style: UITableViewStyle = .plain
+    
+    public var clearsSelectionOnViewWillAppear: Bool = true
+    
+    public var tableView: UITableView! {
+        
+        return self.view as? UITableView
+    }
+    
+    open override func loadView() {
+        
+        let tableView = UITableView.init(frame: CGRect(x: 0, y: 0, width: 320, height: 480), style: style)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        self.view = tableView
+    }
+    
+    public init(style: UITableViewStyle) {
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        self.style = style
+    }
+    
+    public init() {
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    open override var description: String {
+        
+        return String(format: "<%@: %p; tableView = %@>", self.className, NSStringFromClass(type(of: self)), Unmanaged<UITableViewController>.passUnretained(self).toOpaque().debugDescription, self.tableView)
+    }
+    
+    open func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        fatalError("\(#function) not implemented")
+    }
+    
+    open func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) { }
+    
+    open func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) { }
+    
+    open func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) { }
+    
+    open func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) { }
+    
+    open func tableView(_ tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int) { }
+    
+    open func tableView(_ tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) { }
+    
+    open func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { return tableView.rowHeight }
+    
+    //open func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat { return tableView.sectionHeaderHeight }
+    
+    //open func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat { return tableView.sectionFooterHeight }
+    
+    open func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat { return tableView.estimatedRowHeight }
+    
+    open func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat { return tableView.estimatedSectionHeaderHeight }
+    
+    open func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat { return tableView.estimatedSectionFooterHeight }
+    
+    open func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? { return nil }
+    
+    open func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView?  { return nil }
+    
+    open func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) { }
+    
+    open func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool  { return true }
+    
+    open func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) { }
+    
+    open func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath) { }
+    
+    open func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath?  { return indexPath }
+    
+    open func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath? { return indexPath }
+    
+    open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    
+    open func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) { }
+    
+    open func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle { return .delete }
+    
+    open func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? { return nil }
+    
+    open func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? { return nil }
+    
+    open func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool { return true }
+    
+    open func tableView(_ tableView: UITableView, willBeginEditingRowAt indexPath: IndexPath) { }
+    
+    open func tableView(_ tableView: UITableView, didEndEditingRowAt indexPath: IndexPath?) { }
+    
+    open func tableView(_ tableView: UITableView,
+                   targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath,
+                   toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        
+        return proposedDestinationIndexPath
+    }
+}
