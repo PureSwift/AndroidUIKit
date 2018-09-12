248603bc1ab106f4b906b62a9f470c101d1356a7
jmarkstar <mestrella.1991@gmail.com>
2018-09-12 15:04:20 -0500

    Worked on UILabel

--- Sources/UILabel.swift
+++ Sources/UILabel.swift
@@ -19,15 +19,15 @@ open class UILabel: UIView {
             else { fatalError("Missing context") }
         
         let textview = AndroidTextView(context: context)
-        
+        textview.gravity = AndroidGravity.startCenterVertical.rawValue
         textview.color = UIColor.black.androidColor.color
         
         return textview
-    }()
+        }()
     
     //internal var androidTextView: AndroidTextView?
     
-    internal let androidTextViewId = Int.random()
+    internal let androidTextViewId = AndroidViewCompat.generateViewId()
     
     public var text: String? {
         set {
@@ -44,6 +44,17 @@ open class UILabel: UIView {
         }
         get {
             return UIColor.init(color: androidTextView.color)
+        }
+    }
+    
+    /// The technique to use for aligning the text.
+    public var textAlignment: NSTextAlignment {
+        get {
+            return .left
+        }
+        set{
+            
+            androidTextView.gravity = getGravityFromNSTextAlignment(newValue)
         }
     }
     
@@ -64,4 +75,54 @@ open class UILabel: UIView {
         // set size
         androidTextView.layoutParams = AndroidViewGroupLayoutParams(width: Int(frameDp.width), height: Int(frameDp.height))
     }
+    
+    private func getGravityFromNSTextAlignment(_ textAlignment: NSTextAlignment) -> Int {
+        
+        switch textAlignment {
+        case NSTextAlignment.left:
+            return AndroidGravity.startCenterVertical.rawValue
+        case NSTextAlignment.right:
+            return AndroidGravity.endCenterVertical.rawValue
+        case NSTextAlignment.center:
+            return AndroidGravity.center.rawValue
+        default:
+            return AndroidGravity.startCenterVertical.rawValue
+        }
+    }
+    
+    private func getNSTextAlignmentFromGravity(_ gravity: AndroidGravity) -> NSTextAlignment {
+        
+        switch gravity {
+        case AndroidGravity.startCenterVertical:
+            return NSTextAlignment.left
+        case AndroidGravity.endCenterVertical:
+            return NSTextAlignment.right
+        case AndroidGravity.center:
+            return NSTextAlignment.center
+        default:
+            return NSTextAlignment.left
+        }
+    }
 }
+
+/// These constants specify text alignment.
+public enum NSTextAlignment {
+    
+    /// Text is visually left aligned.
+    case left
+    
+    /// Text is visually right aligned.
+    case right
+    
+    /// Text is visually center aligned.
+    case center
+    
+    /// Text is justified.
+    case justified
+    
+    /// Use the default alignment associated with the current localization of the app.
+    /// The default alignment for left-to-right scripts is NSTextAlignment.left,
+    /// and the default alignment for right-to-left scripts is NSTextAlignment.right.
+    case natural
+}
+
